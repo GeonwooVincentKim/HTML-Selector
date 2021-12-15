0377192d49faa29b57b26c5ad62f8eefceff8afd
GeonwooVincentKim <kdsnop@gmail.com>
2021-12-15 16:53:28 +0900

    [Feature] - Match Text

--- main.js
+++ main.js
@@ -6,16 +6,22 @@ const container = document.querySelector(".container");
 
 selected.addEventListener("click", () => {
   optionsContainer.classList.toggle("active");
+  console.log(selected.innerHTML);
+  if(selected.innerHTML.match("Select Video Category")) {
+    selected.style.color = "#fff";
+  }
 });
 
 optionsList.forEach(o => {
   o.addEventListener("click", () => {
     selected.innerHTML = o.querySelector("label").innerHTML;
+    if(!(selected.innerHTML.match("Select Video Category"))) {
+      selected.style.color = "#000";
+    }
     optionsContainer.classList.remove("active");
   });
 });
 
 $(document).mouseup(function (e){
   optionsContainer.classList.remove("active");
-  // selected.innerHTML = o.querySelector("label").innerHTML;
 });
