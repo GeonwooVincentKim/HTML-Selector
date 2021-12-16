86579cc31ea6fd5f5c5fe38f95ac5fd3c2d151d8
GeonwooVincentKim <kdsnop@gmail.com>
2021-12-16 16:03:36 +0900

    test

--- main.js
+++ main.js
@@ -2,27 +2,37 @@ const selected = document.querySelector(".selected");
 const optionsContainer = document.querySelector(".options-container");
 
 const optionsList = document.querySelectorAll(".option");
-const container = document.querySelector(".container");
 
 selected.addEventListener("click", () => {
-  optionsContainer.classList.toggle("active");
-  console.log(selected.innerHTML);
-  if(selected.innerHTML.match("Select Video Category")) {
-    selected.style.color = "#fff";
-  }
+    optionsContainer.classList.toggle("active");
+    console.log(selected.innerHTML);
+    if(selected.innerHTML.match("Select Video Category")) {
+        selected.style.color = "#fff";
+    }
 });
 
+// var getOptionValue = selected.innerHTML;
+// console.log(getOptionValue.name);
+
 optionsList.forEach(o => {
-  o.addEventListener("click", () => {
-    selected.innerHTML = o.querySelector("label").innerHTML;
-    if(!(selected.innerHTML.match("Select Video Category"))) {
-      selected.style.color = "#000";
-    }
-    optionsContainer.classList.remove("active");
-  });
+    var getOptionValue;
+    o.addEventListener("click", () => {
+        selected.innerHTML = o.querySelector("label").innerHTML;
+        if(!(selected.innerHTML.match("Select Video Category"))) {
+            selected.style.color = "#000";
+        } 
+        o.style.background = "rgba(104, 194, 121, 0.5)";
+        o.style.borderBottom = "1px solid #ebebeb"
+        o.style.fontWeight = "bold";
+        optionsContainer.classList.remove("active");
+    });
 });
 
 $(document).mouseup(function (e){
-  optionsContainer.classList.remove("active");
-  selected.style.color = "#000";
+    optionsContainer.classList.remove("active");
+    if(selected.innerHTML.match("Select Video Category")) {
+        selected.style.color = "#999";
+    } else {
+        selected.style.color = "#000";
+    }
 });
