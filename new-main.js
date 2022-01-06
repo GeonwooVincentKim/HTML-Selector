2b77af8f72592c38086e476da540577e7b08f0ce
GeonwooVincentKim <kdsnop@gmail.com>
2022-01-06 13:53:10 +0900

    [Update] - Selected Features

--- new-main.js
+++ new-main.js
@@ -1,7 +1,38 @@
 const check_selected = document.querySelector(".check-selected");
 const new_optionsContainer = document.querySelector(".new-options-container");
 
+const new_optionsList = document.querySelectorAll(".option");
+
 check_selected.addEventListener("click", () => {
     new_optionsContainer.classList.toggle("new-active");
     console.log(check_selected.innerHTML);
+    
+    if(check_selected.innerHTML.match("Default"))
+        check_selected.style.color = "#fff";
 });
+
+new_optionsList.forEach(o => {
+    var getOptionValue;
+    o.addEventListener("click", (e) => {
+        check_selected.innerHTML = o.querySelector("label").innerHTML;
+        if(!(check_selected.innerHTML.match("Select Video Category"))) {
+            check_selected.style.color = "#000";
+        }
+
+        const previousState = document.querySelector(".option.check-selected");
+        if(previousState) previousState.classList.remove("check-selected");
+        e.currentTarget.classList.add("check-selected");
+        
+        new_optionsContainer.classList.remove("new-active");
+    });
+});
+
+$(document).mouseup(function (e){
+    new_optionsContainer.classList.remove("new-active");
+    if(check_selected.innerHTML.match("Select Video Category")) {
+        check_selected.style.color = "#999";
+    } else {
+        check_selected.style.color = "#000";
+    }
+});
+
